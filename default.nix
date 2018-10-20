7ebbdd91e56aeed687839bacc14a0b7406428bd5
Jakub Zárybnický <jakub.zarybnicky@inuits.eu>
2018-10-20 23:42:04 +0200

    Basic site

---
+++ default.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  filterSite = path: _:
+    let baseName = baseNameOf (toString path); in
+    baseName != "bootstrap" &&
+    baseName != "scss" &&
+    baseName != "default.nix" &&
+    baseName != "result" &&
+    baseName != ".git" &&
+    baseName != ".gitignore" &&
+    baseName != ".gitmodules" &&
+    baseName != ".sass-cache";
+in {
+  site = pkgs.stdenv.mkDerivation rec {
+    name = "olomouc.dance";
+    src = builtins.filterSource filterSite ./.;
+    phases = "unpackPhase buildPhase";
+    buildPhase = ''
+      mkdir -p $out
+      cp -r ./* $out
+    '';
+  };
+}
